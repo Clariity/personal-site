292bff5575579e0948dd400efdfa164d69d8a0a3
Ryan Gregory <ryangregory.dev@outlook.com>
2026-09-14 21:04:27 +0100

    fix: build prerender failure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,9 +25,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Analytics />
-
       <body className="min-h-full flex flex-col bg-background text-foreground container mx-auto px-8 lg:px-16">
+        <Suspense fallback={null}>
+          <Analytics />
+        </Suspense>
+
         {children}
 
         <hr />
